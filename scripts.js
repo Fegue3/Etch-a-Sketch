45cf28c46a5fdd77c16b586f2719ca1c9ce0db9c
Francisco <fp226766@gmail.com>
2025-01-22 17:40:45 +0000

    added the constants variables

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const newgrid_button = document.querySelector("#newgrid");
+const resetbutton = document.querySelector("#resetButton");
+const colorpicker = document.querySelector("#color-picker");
+const fillgridcolor = document.querySelector("#fillgrid");
+const rainbowcolor = document.querySelector("#rainbowbutton");
+const container = document.querySelector("#container");
